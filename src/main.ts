4640d5497b5075a322720e2af675862f9dd7fafb
sebastien baudet <seb.baudet76@gmail.com>
2022-07-05 19:37:44 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+dotenv.config();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Info Line')
+    .setDescription("Description de l'API Info Line ")
+    .setVersion('1.0')
+    .addTag('infoLine')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
