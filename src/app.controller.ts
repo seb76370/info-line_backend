a3d4ac2d10b3565e2177fa26b139ace759572786
Kevin.Bonnet <103254324+kkevin203@users.noreply.github.com>
2022-07-14 19:49:58 +0200

    update token header

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Headers, UnauthorizedException } from '@nestjs/common';
+import { log } from 'console';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,9 +7,16 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    console.log("le controller");
-    
+  getHello(@Headers('Authorization') authorization = ''): string {
+    let bearer: string = '';
+
+    if (typeof authorization != 'undefined') {
+      bearer = authorization.replace('Bearer ', '');
+    }
+    if (bearer === '') {
+      throw new UnauthorizedException('No Token provided!');
+    }
+    console.log(bearer);
     return this.appService.getHello();
   }
 }
--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,11 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth/auth.service'
+
 @Injectable()
 export class LoginService {
     constructor(private readonly authService: AuthService) {}
     
     async login(body: Record<string,any>) {
         //TODO verifier login/mot de passe dans la base de donnée auprès des repository     
-        this.authService.auth(body)       
+        const userLoged = false
+    if (userLoged) {
+        this.authService.auth(body)    
+    } else {
+      throw new UnauthorizedException('Incorrect login credentials!');
+    }
     }
 }
